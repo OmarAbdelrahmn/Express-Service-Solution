f95af7af8ea4a409275dbdf7ff24e463d5ed6a1a
omar abdo <omarfaroq2003@gmail.com>
2026-07-30 10:45:56 +0300

    remove the block of the employee files

--- Express Service/Program.cs
+++ Express Service/Program.cs
@@ -146,8 +146,7 @@ RecurringJob.AddOrUpdate<IAbsentReportJob>(
 
 app.Use(async (context, next) =>
 {
-    if (context.Request.Path.StartsWithSegments("/uploads", StringComparison.OrdinalIgnoreCase) ||
-        context.Request.Path.StartsWithSegments("/vacation-documents", StringComparison.OrdinalIgnoreCase))
+    if (context.Request.Path.StartsWithSegments("/vacation-documents", StringComparison.OrdinalIgnoreCase))
     {
         context.Response.StatusCode = StatusCodes.Status404NotFound;
         return;
